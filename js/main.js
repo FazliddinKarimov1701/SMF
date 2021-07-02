a76e64b145ec4a1c6a5ba135e365a9eb5454179d
FazliddinKarimov1701 <fazliddinkarimov1701@gmail.com>
2021-07-02 22:30:06 +0500

    Working on testimonials section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// $(function(){
+//     $("#customers-testimonial").owlCarousel({
+//         items:1,
+//         autoplay:true,
+//         smartSpeed: 700,
+//         loop:true,
+//         autoPlayHoverPause:true
+//     });
+// });
